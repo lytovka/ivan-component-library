6f8597ee01230f7bfae38f072232147d268a74b3
Ivan Lytovka <ivanlytovka@gmail.com>
2021-02-13 16:45:47 -0500

    feat: add react-testing-library to project

---
+++ src/TestComponent.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import TestComponent from "./TestComponent";
+import { TestComponentProps } from "./TestComponent.types";
+
+describe("Test Component", () => {
+  let props: TestComponentProps;
+
+  beforeEach(() => {
+    props = {
+      $color: "red"
+    };
+  });
+
+  const renderComponent = () => render(<TestComponent data-testid="heading" {...props} />);
+
+  it("should render Heading with red color", () => {
+    const { getByTestId } = renderComponent();
+    expect(getByTestId('heading')).toHaveStyle("color: red")
+  });
+
+  it("should render Heading with green color", () => {
+    props.$color = "green";
+    const { getByTestId } = renderComponent();
+
+    expect(getByTestId('heading')).toHaveStyle("color: green")
+  });
+});
